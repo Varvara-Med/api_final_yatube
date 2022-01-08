1ac603b7db344d3fb07cf57a9285da93baaf95df
Varya <varvara.kireeva@gmail.com>
2022-01-08 15:02:50 +0200

    Tests passed

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -8,8 +8,10 @@ router = DefaultRouter()
 router.register(r'posts', PostViewSet, basename='posts')
 router.register(r'users', UserViewSet, basename='users')
 router.register(r'groups', GroupViewSet, basename='groups')
-router.register(r'posts/(?P<post_id>\d+)/coments', CommentViewSet,
-                basename='comments')
+router.register(r'posts/(?P<post_id>\d+)/comments',
+                CommentViewSet,
+                basename='comments'
+                )
 router.register(r'follow', FollowViewSet, basename='follow')
 
 urlpatterns = [
