3dc5c3a7fd6236374a28764e284544f37f828778
Varvara-Med <90317295+Varvara-Med@users.noreply.github.com>
2022-03-22 13:28:02 +0200

    Done

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,7 +7,7 @@ sys.path.append(root_dir)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 root_dir_content = os.listdir(BASE_DIR)
 PROJECT_DIR_NAME = 'yatube_api'
-# проверяем, что в корне репозитория лежит папка с проектом
+"""Проверяем, что в корне репозитория лежит папка с проектом."""
 if (
         PROJECT_DIR_NAME not in root_dir_content
         or not os.path.isdir(os.path.join(BASE_DIR, PROJECT_DIR_NAME))
@@ -20,7 +20,7 @@ if (
 MANAGE_PATH = os.path.join(BASE_DIR, PROJECT_DIR_NAME)
 project_dir_content = os.listdir(MANAGE_PATH)
 FILENAME = 'manage.py'
-# проверяем, что структура проекта верная, и manage.py на месте
+"""Проверяем, что структура проекта верная, и manage.py на месте."""
 if FILENAME not in project_dir_content:
     assert False, (
         f'В директории `{MANAGE_PATH}` не найден файл `{FILENAME}`. '
@@ -32,7 +32,6 @@ pytest_plugins = [
     'tests.fixtures.fixture_data',
 ]
 
-# test .md
 default_md = '# api_final\napi final\n'
 filename = 'README.md'
 assert filename in root_dir_content, (
